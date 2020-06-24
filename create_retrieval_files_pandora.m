e23d5522d7c2a254730de23e12f803be57c54713
KristofBognar <kbognar@physics.utoronto.ca>
2020-06-24 09:29:53 -0400

    finalised input file creation

--- create_retrieval_files_pandora.m
+++ create_retrieval_files_pandora.m
@@ -1,48 +1,52 @@
-function create_retrieval_files_pandora(pnum,uvvis)
+function create_retrieval_files_pandora(out_folder_name,dscd_fname,date_in,daily_times,fpath)
 % Create daily retrieval settings file for pandora aerosol and NO2 retrievals
 %
-% dSCD files created by reformat_pandora_for_retrievals.m and write_input_HEIPRO.m
+% % % % dSCD files created by reformat_pandora_for_retrievals.m and write_input_HEIPRO.m
+% % % %
+% % % % Enter start-end times and intervals manually, so files are checked over before the
+% % % % retrievals; there are too many exceptions and issues for automating the process
+% % % %   
+% % % % Aerosol a priori is fixed
+% % % % BrO a priori determined from BrO dSCDs and T inversion
+% % % %       Surface conc.: 5 when 5deg dscd consistently above 1e14
+% % % %       Scale height: from get_sonde_PT.m (check T profile plots as well)
+% % % % 
+% % % % retrieval performed on Windows side; separate code (python) modifies
+% % % % yealy control file with sonde filenames, and copies appropriate retrieval
+% % % % file to xx_retrieval.inp
 %
-% Enter start-end times and intervals manually, so files are checked over before the
-% retrievals; there are too many exceptions and issues for automating the process
-%   
-% Aerosol a priori is fixed
-% BrO a priori determined from BrO dSCDs and T inversion
-%       Surface conc.: 5 when 5deg dscd consistently above 1e14
-%       Scale height: from get_sonde_PT.m (check T profile plots as well)
-% 
-% retrieval performed on Windows side; separate code (python) modifies
-% yealy control file with sonde filenames, and copies appropriate retrieval
-% file to xx_retrieval.inp
+% @Kristof Bognar, June 2020
 %
-% @Kristof Bognar, sometime in 2018
-%
-%% setup
 
-% aer=0; % 1 for aerosol, 0 for BrO
-% year=2018;
+if out_folder_name(end)=='/', out_folder_name=out_folder_name(1:end-1); end
 
-%%%%%%%%%%%%% do it as a function of pnum and uvvis
-error('to do: out folder automation, date/time selection, dscd file naming')
+%% instrument altitude (km above surface, not sea level)
 
-out_folder_name=['p' num2str(pnum) '_' uvvis];
+p_num=str2double(out_folder_name(2:4));
 
+if any(p_num==[103,104])
+ 
+    % ECCC Downsview: 15m
+    instr_alt_str='0.015';
+    loc_str='Downsview';
+        
+else
+    error(['Add location details for Pandora ' num2str(instr_num)]);
+end
 
-% start/stop times and BrO a priori are selected manually, and saved
-%%%%%%%%%%%%%%%%[dates,daily_times,apriori_BrO] = variable_init(year);
-
-%% read template
+%% do both aerosol and tg input
 for aer=[1,0]
-    
+    %% read template
     if aer
         fid=fopen(['/media/kristof/Windows7_OS/SCIATRAN2/AEROSOL_RETRIEVAL_v-1-2/',...
                        'IDL_execute/aerosol_retrieval_template_pandora_vis.inp'],'r');
     else
         fid=fopen(['/media/kristof/Windows7_OS/SCIATRAN2/TRACEGAS_RETRIEVAL_v-1-2/',...
-                       'IDL_execute/tracegas_retrieval_template_pandora vis.inp'],'r');
+                       'IDL_execute/tracegas_retrieval_template_pandora_vis.inp'],'r');
     end
 
     i = 1;
+    infile={};
     tline = fgetl(fid);
     infile{i} = tline;
     while ischar(tline)
@@ -52,226 +56,80 @@ for aer=[1,0]
     end
     fclose(fid);
 
-    %% loop over desired dates
-    for i=1:length(dates)
+    outfile=infile;
 
-        outfile=infile;
+    %% modify relevant lines
 
-        %% modify relevant lines
+    %%% look for field description for dates/times, since placeholder and field
+    %%% header repeat
 
-        %%% look for field description for dates/times, since placeholder and field
-        %%% header repeat
+    % start date
+    tmp=find_in_file(outfile,'Start day of considered time period');
+    outfile{tmp+2}=datestr(date_in,'dd/mm/yyyy');
 
-        % start date
-        tmp=find_in_file(outfile,'Start day of considered time period');
-        outfile{tmp+2}=datestr(dates(i),'dd/mm/yyyy');
+    % stop date
+    tmp=find_in_file(outfile,'Stop day of considered time period');
+    outfile{tmp+2}=datestr(date_in,'dd/mm/yyyy');
 
-        % stop date
-        tmp=find_in_file(outfile,'Stop day of considered time period');
-        outfile{tmp+2}=datestr(dates(i),'dd/mm/yyyy');
+    % start time
+    tmp=find_in_file(outfile,'Start time for the considered day');
+    outfile{tmp+2}=daily_times{1};
 
-        % start time
-        tmp=find_in_file(outfile,'Start time for the considered day');
-        outfile{tmp+2}=daily_times{i,1};
+    % stop time
+    tmp=find_in_file(outfile,'Stop time for the considered day');
+    outfile{tmp+2}=daily_times{2};
 
-        % stop time
-        tmp=find_in_file(outfile,'Stop time for the considered day');
-        outfile{tmp+2}=daily_times{i,2};
+    % time interval
+    tmp=find_in_file(outfile,'Interval length for the temporal retrieval sequences');
+    outfile{tmp+2}=daily_times{3};
 
-        % time interval
-        tmp=find_in_file(outfile,'Interval length for the temporal retrieval sequences');
-        outfile{tmp+2}=daily_times{i,3};
+    %%% output folder
+    tmp=find_in_file(outfile,'Folder for the output (will be generated if necessary)');
+    outfile{tmp+2}=out_folder_name;
 
-        %%% output folder
-        tmp=find_in_file(outfile,'Folder for the output (will be generated if necessary)');
-        outfile{tmp+2}=out_folder_name;
+    %%% instrument position
+    tmp=find_in_file(outfile,'# Height position of instrument in [km]');
+    outfile{tmp+2}=instr_alt_str;
+    
+    %%% look for placeholder for dSCD and sonde files
 
-        %%% look for placeholder for dSCD and sonde files
+    % dSCD input file
+    tmp=find_in_file(outfile,'dscd_xxxx.dat');
+    outfile{tmp}=dscd_fname;
 
-        % dSCD input file
-        tmp=find_in_file(outfile,'dscdxxxx.dat');
-    % % %     outfile{tmp}=['DSCD_' datestr(dates(i),'yyyy_mm_dd') '.dat'];
+    % radiosonde input file (indices specific to path!!)
+    tmp=find_in_file(outfile,'ERA5_xxxxxx.dat');
+%     outfile{tmp}(end-9:end-4)=datestr(date_in,'yymmdd');
+    outfile{tmp}=['C:\SCIATRAN2\AEROSOL_RETRIEVAL_v-1-2\Campaign\Complement_Data\Radiosonde\' ...
+                  'ERA5_' loc_str '_' datestr(date_in,'yymmdd') '.dat'];
 
-        % radiosonde input file (indices specific to path!!)
-        tmp=find_in_file(outfile,'radiosonde_xxxxxx.dat');
-    %     outfile{tmp}(end-9:end-4)=datestr(dates(i),'yymmdd');
-
-        % directory with aerosol results for tg retrieval
-        if ~aer
-            tmp=find_in_file(outfile,'Path to the aerosol retrieval results');
-            outfile{tmp+3}=['C:\SCIATRAN2\AEROSOL_RETRIEVAL_v-1-2\Campaign\'...
-                            out_folder_name '\'];
-        end
-
-
-        %% write file
-
-        fpath=['/home/kristof/Drive/data_for_profile_retrieval/input_files/pandora/' ...
-               out_folder_name '/'];
-
-        if ~exist(fpath,'dir'), mkdir(fpath); end
-
-        if aer
-            fname=['aerosol_retrieval_' out_folder_name '_' datestr(dates(i),'yymmdd') '.inp'];
-        else
-            fname=['tracegas_retrieval_' out_folder_name '_' datestr(dates(i),'yymmdd') '.inp'];
-        end
-
-        file_tmp = fopen([fpath fname],'w');
-        fprintf(file_tmp,'%s\n',outfile{:});
-        fclose(file_tmp);        
-
-
+    % directory with aerosol results for tg retrieval
+    if ~aer
+        tmp=find_in_file(outfile,'Path to the aerosol retrieval results');
+        outfile{tmp+3}=['C:\SCIATRAN2\AEROSOL_RETRIEVAL_v-1-2\Campaign\'...
+                        out_folder_name '\'];
     end
+
+
+    %% write file
+
+%         fpath=['/home/kristof/Drive/data_for_profile_retrieval/input_files/pandora/' ...
+%                out_folder_name '/'];
+
+    if ~exist(fpath,'dir'), mkdir(fpath); end
+
+    if aer
+        fname=['aerosol_retrieval_' out_folder_name '_' datestr(date_in,'yymmdd') '.inp'];
+    else
+        fname=['tracegas_retrieval_' out_folder_name '_' datestr(date_in,'yymmdd') '.inp'];
+    end
+    
+    file_tmp = fopen([fpath fname],'w');
+    fprintf(file_tmp,'%s\n',outfile{:});
+    fclose(file_tmp);        
+
     
 end
 
 end
-
-
-
-function [dates,daily_times,apriori_BrO] = variable_init(year)
-
-    %%% start and end times + time interval (match manually to times in dSCD file)
-    % (start,stop,interval, mmdd, comments), and
-    %%% BrO a priori
-    % determined by visual insection of dSCDs and T inversion (Zhao et al., 2016)
-    % surf VMR (x1e-6), scake height (km), mmdd
-        
-    if year==2019
-        
-        % day range 
-        doy_range=[64:151]; 
-
-        % remove missing days
-        missing=[112,118];
-        doy_range=setdiff(doy_range, missing);
-
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-        
-        % times and apriori info        
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2019.mat')
-       
-    end
-    
-    if year==2018
-        %% dates
-
-        % day range 
-        doy_range=[64:151]; 
-%         doy_range=[64:151,213:216,218:221,233,234]; 
-
-        % remove missing days
-        doy_range(doy_range==133)=[]; 
-        
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-
-        % times and apriori info        
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2018.mat')
-                 
-    elseif year==2017
-        %% dates
-
-        % day range 
-        doy_range=[66:94]; 
-
-        % remove missing days
-%         doy_range(doy_range==133)=[]; 
-
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-        
-        % times and apriori info
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2017.mat')       
-   
-    elseif year==2016
-        %% dates
-
-        % day range 
-        doy_range=[66:132]; 
-
-        % remove missing days
-        missing=[73:75,84:91];
-        doy_range=setdiff(doy_range, missing);
-
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-
-        % times and apriori info
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2016.mat')       
-
-    elseif year==2015
-        %% dates
-
-        % day range 
-        doy_range=[62:151]; 
-
-        % remove missing days
-        missing=[63,87,98,99,106,123,145];
-        doy_range=setdiff(doy_range, missing);
-
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-
-        % times and apriori info
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2015.mat')       
-      
-    elseif year==2013
-        %% dates
-
-        % day range 
-        doy_range=[69:113]; 
-
-        % remove missing days
-        doy_range(doy_range==93)=[]; 
-
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-                
-        % times and apriori info
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2013.mat')       
-
-    elseif year==2011
-        %% dates
-
-        % day range 
-        doy_range=[70:100]; 
-
-        % remove missing days
-        missing=[83,84,96,98];
-        doy_range=setdiff(doy_range, missing);
-
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-                
-        % times and apriori info
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2011.mat')       
-        
-    elseif year==2010
-        %% dates
-
-        % day range 
-        %%% there are more measurements; only process part of the dataset
-        doy_range=[100:140]; 
-
-        % remove days that are not needed
-        missing=[107:122,130];
-        doy_range=setdiff(doy_range, missing);
-
-        % dates
-        dates=ft_to_date(doy_range-1,year);
-                
-        % times and apriori info
-        % used 1h profile time window for most days, since scans are very
-        % long for some reason (there are lots of gaps too)
-        load('/home/kristof/work/profile_retrievals/profile_results/profile_details/prof_info_2010.mat')       
-        
-
-    end
-end
-
-
-
 
--- write_input_HEIPRO.m
+++ write_input_HEIPRO.m
@@ -1,4 +1,4 @@
-function write_input_HEIPRO( p_num, uvvis, yr_in, file_dir, out_type, split_scans )
+function write_input_HEIPRO( p_num, uvvis, yr_in, file_dir, split_scans )
 % Write Pandora dSCDs to input files used by HEIPRO
 %
 %%   
@@ -11,16 +11,19 @@ function write_input_HEIPRO( p_num, uvvis, yr_in, file_dir, out_type, split_scan
 %                  processing (used for output file name only)
 %           yr_in: year of data to process
 %           file_dir: directory where processed data is found
-%           out_type: 'day', 'week', or 'month' for daily, weekly or monthly files
 %           split_scans: 0 to write all scans into one file
 %                        1 to separate short and long scans into different files
 %                        2 to do 1 and 2 at the same time
 %
-% OUTPUT:   daily/weekly/monthly dSCD files with dummy 90 deg lines inserted
+% OUTPUT:   daily dSCD files with dummy 90 deg lines inserted
 %           Data is saved in a new folder in file_dir
 %
 %
 % Kristof Bognar, August 2019
+
+% fix daily output, code to find dt and start/end times works for daily
+% input only
+out_type='day';
 
 %% set input/output folder naming
 if ~strcmp(file_dir(end),'/'), file_dir=[file_dir, '/']; end
@@ -68,10 +71,12 @@ table_in=pan_maxdoas_processed;
 
 % create output folder
 % data is saved separately for each instument and wavelength range
-savedir=[file_dir 'HEIPRO_input/' output_version];
+savedir_dscd=[file_dir 'HEIPRO_input/' output_version 'dSCD_files/'];
+savedir_inp=[file_dir 'HEIPRO_input/' output_version 'input_files/'];
 
 % create folder if necessary
-if ~exist(savedir,'dir'), mkdir(savedir), end
+if ~exist(savedir_dscd,'dir'), mkdir(savedir_dscd), end
+if ~exist(savedir_inp,'dir'), mkdir(savedir_inp), end
 
 switch out_type
     case 'day' % get DOY (fractionalday in table is not necessarily correct...)
@@ -86,10 +91,12 @@ switch out_type
 end
      
 % loop over days/weeks/months
-for i=unique(time_steps)'
+steps_list=unique(time_steps);
+
+for i=1:length(steps_list)
 
     % select data from given period
-    to_write=table_in(time_steps==i,:);
+    to_write=table_in(time_steps==steps_list(i),:);
 
     % discard partial scans before first and after last 90deg dummy
     % there are a very large number of scans even for each day, no point
@@ -107,41 +114,50 @@ for i=unique(time_steps)'
         %%% long scans
         to_write_tmp=to_write(to_write.longscan==1,:);
         
-        % output file name
-        fname=[savedir 'long_p' num2str(p_num) '_' uvvis '_' num2str(yr_in)...
-               '_' out_type num2str(i) '.dat'];
+        % get dt and start/end times
+        [ daily_times, to_write_tmp ] = find_dt_HEIPRO(to_write_tmp);
         
+        % output file name
+        dscd_fname=['long_p' num2str(p_num) '_' uvvis '_' num2str(yr_in)...
+               '_' out_type num2str(steps_list(i)) '.dat'];
+        fname=[savedir_dscd dscd_fname];
+        
+        % write dscd file
         to_write_tmp.longscan=[];
         to_write_tmp.shortscan=[];
         writetable(to_write_tmp,fname,'Delimiter',',');
         
+        % create heipro control files
+        date_in=to_write_tmp.DateTime(1);
+        create_retrieval_files_pandora(output_version,dscd_fname,date_in,daily_times,savedir_inp);
         
-        %%% short scans
-        to_write_tmp=to_write(to_write.shortscan==1,:);
-        
-        % output file name
-        fname=[savedir 'short_p' num2str(p_num) '_' uvvis '_' num2str(yr_in)...
-               '_' out_type num2str(i) '.dat'];
-        
-        to_write_tmp.longscan=[];
-        to_write_tmp.shortscan=[];
-        writetable(to_write_tmp,fname,'Delimiter',',');
+% %         %%% short scans
+% %         to_write_tmp=to_write(to_write.shortscan==1,:);
+% %         
+% %         % output file name
+% %         fname=[savedir 'short_p' num2str(p_num) '_' uvvis '_' num2str(yr_in)...
+% %                '_' out_type num2str(steps_list(i)) '.dat'];
+% %         
+% %         to_write_tmp.longscan=[];
+% %         to_write_tmp.shortscan=[];
+%         writetable(to_write_tmp,fname,'Delimiter',',');
         
     end
     
     if split_scans~=1
-        % write all scans in one file
-        
-        % output file name
-        fname=[savedir 'p' num2str(p_num) '_' uvvis '_' num2str(yr_in)...
-               '_' out_type num2str(i) '.dat'];
-        
-        to_write.longscan=[];
-        to_write.shortscan=[];
-        writetable(to_write,fname,'Delimiter',',');
+% %         % write all scans in one file
+% %         
+% %         % output file name
+% %         fname=[savedir 'p' num2str(p_num) '_' uvvis '_' num2str(yr_in)...
+% %                '_' out_type num2str(steps_list(i)) '.dat'];
+% %         
+% %         to_write.longscan=[];
+% %         to_write.shortscan=[];
+% %         writetable(to_write,fname,'Delimiter',',');
 
     end
         
 end
         
 end
+
